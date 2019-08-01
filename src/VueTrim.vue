92a604456e074311b258b7b7eb61253d90f44c51
1000ch <shogo.sensui@gmail.com>
2019-08-01 18:37:37 +0900

    feat: accept all options

--- src/VueTrim.vue
+++ src/VueTrim.vue
@@ -9,6 +9,47 @@
 <script>
 import Cropper from 'cropperjs';
 
+const cropperOptions = [
+  'viewMode',
+  'dragMode',
+  'initialAspectRatio',
+  'aspectRatio',
+  'data',
+  'responsive',
+  'restore',
+  'checkCrossOrigin',
+  'checkOrientation',
+  'modal',
+  'guides',
+  'center',
+  'highlight',
+  'background',
+  'autoCrop',
+  'autoCropArea',
+  'movable',
+  'rotatable',
+  'scalable',
+  'zoomable',
+  'zoomOnTouch',
+  'zoomOnWheel',
+  'wheelZoomRatio',
+  'cropBoxMovable',
+  'cropBoxResizable',
+  'toggleDragModeOnDblclick',
+  'minContainerWidth',
+  'minContainerHeight',
+  'minCanvasWidth',
+  'minCanvasHeight',
+  'minCropBoxWidth',
+  'minCropBoxHeight',
+  'ready',
+  'cropstart',
+  'cropmove',
+  'cropend',
+  'crop',
+  'zoom'
+];
+
 const tagValues = [
   'img',
   'canvas'
@@ -47,7 +88,133 @@ export default {
       default: 'crop',
       type: String,
       validator: value => dragModeValues.includes(value)
+    },
+    initialAspectRatio: {
+      default: NaN,
+      type: Number
+    },
+    aspectRatio: {
+      default: NaN,
+      type: Number
+    },
+    data: {
+      default: {},
+      type: Object
+    },
+    responsive: {
+      default: true,
+      type: Boolean
+    },
+    restore: {
+      default: true,
+      type: Boolean
+    },
+    checkCrossOrigin: {
+      default: true,
+      type: Boolean
+    },
+    checkOrientation: {
+      default: true,
+      type: Boolean
+    },
+    modal: {
+      default: true,
+      type: Boolean
+    },
+    guides: {
+      default: true,
+      type: Boolean
+    },
+    center: {
+      default: true,
+      type: Boolean
+    },
+    highlight: {
+      default: true,
+      type: Boolean
+    },
+    background: {
+      default: true,
+      type: Boolean
+    },
+    autoCrop: {
+      default: true,
+      type: Boolean
+    },
+    autoCropArea: {
+      default: 0.8,
+      type: Number,
+      validator: value => 0 <= value && value <= 1
+    },
+    movable: {
+      default: true,
+      type: Boolean
+    },
+    rotatable: {
+      default: true,
+      type: Boolean
+    },
+    scalable: {
+      default: true,
+      type: Boolean
+    },
+    zoomable: {
+      default: true,
+      type: Boolean
+    },
+    zoomOnTouch: {
+      default: true,
+      type: Boolean
+    },
+    zoomOnWheel: {
+      default: true,
+      type: Boolean
+    },
+    wheelZoomRatio: {
+      default: 0.8,
+      type: Number
+    },
+    cropBoxMovable: {
+      default: true,
+      type: Boolean
+    },
+    cropBoxResizable: {
+      default: true,
+      type: Boolean
+    },
+    toggleDragModeOnDblclick: {
+      default: true,
+      type: Boolean
+    },
+    minContainerWidth: {
+      default: 200,
+      type: Number
+    },
+    minContainerHeight: {
+      default: 100,
+      type: Number
+    },
+    minCanvasWidth: {
+      default: 0,
+      type: Number
+    },
+    minCanvasHeight: {
+      default: 0,
+      type: Number
+    },
+    minCropBoxWidth: {
+      default: 0,
+      type: Number
+    },
+    minCropBoxHeight: {
+      default: 0,
+      type: Number
     }
+  },
+  data() {
+    return {
+      cropper: null
+    };
   },
   watch: {
     src() {
@@ -59,15 +226,34 @@ export default {
   },
   methods: {
     initialize() {
-      const {
-        viewMode,
-        dragMode
-      } = this;
+      if (this.cropper !== null) {
+        this.cropper.destroy();
+      }
 
-      const cropper = new Cropper(this.$refs.img, {
-        viewMode,
-        dragMode
-      });
+      const options = {};
+      for (const cropperOption of cropperOptions) {
+        options[cropperOption] = this[cropperOption];
+      }
+
+      this.cropper = new Cropper(this.$refs.img, options);
+    },
+    ready(e) {
+      this.$emit('ready', e);
+    },
+    cropstart(e) {
+      this.$emit('cropstart', e);
+    },
+    cropmove(e) {
+      this.$emit('cropmove', e);
+    },
+    cropend(e) {
+      this.$emit('cropend', e);
+    },
+    crop(e) {
+      this.$emit('crop', e);
+    },
+    zoom(e) {
+      this.$emit('zoom', e);
     }
   }
 };
