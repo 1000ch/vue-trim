a87614a9c9c939789ea00901985462c9af812279
1000ch <shogo.sensui@gmail.com>
2019-08-03 22:04:21 +0900

    test: update specs

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,5 @@
 import { mount, createLocalVue } from '@vue/test-utils';
+import Cropper from 'cropperjs';
 import VueTrim from '../src';
 
 describe('VueTrim', () => {
@@ -22,5 +23,40 @@ describe('VueTrim', () => {
     }, { localVue });
 
     expect(trim.html()).toEqual('<img alt=\"\" src=\"\">');
+  });
+
+  test('has cropper instance', () => {
+    const localVue = createLocalVue();
+    localVue.use(VueTrim);
+
+    const { vm } = mount({
+      template: `<vue-trim ref="trim" />`
+    }, { localVue });
+
+    expect(vm).toBeDefined();
+    expect(vm.$refs.trim).toBeDefined();
+    expect(vm.$refs.trim.cropper).toBeInstanceOf(Cropper);
+  });
+
+  test('has functions', () => {
+    const localVue = createLocalVue();
+    localVue.use(VueTrim);
+
+    const { vm } = mount({
+      template: `<vue-trim ref="trim" />`
+    }, { localVue });
+
+    expect(vm.$refs.trim.getData).toBeDefined();
+    expect(vm.$refs.trim.setData).toBeDefined();
+    expect(vm.$refs.trim.getContainerData).toBeDefined();
+    expect(vm.$refs.trim.getImageData).toBeDefined();
+    expect(vm.$refs.trim.getImageData).toBeDefined();
+    expect(vm.$refs.trim.getCanvasData).toBeDefined();
+    expect(vm.$refs.trim.setCanvasData).toBeDefined();
+    expect(vm.$refs.trim.getCropBoxData).toBeDefined();
+    expect(vm.$refs.trim.setCropBoxData).toBeDefined();
+    expect(vm.$refs.trim.getCroppedCanvas).toBeDefined();
+    expect(vm.$refs.trim.setAspectRatio).toBeDefined();
+    expect(vm.$refs.trim.setDragMode).toBeDefined();
   });
 });
